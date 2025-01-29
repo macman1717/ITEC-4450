212b02ca45653585ad3fd114bc4053b87aa7f6c5
macman1717 <mchawk1717@gmail.com>
2025-01-29 09:38:55 -0500

    refactored lab3, need to update to force questions answered before submission

--- labs/lab3/lab3.php
+++ labs/lab3/lab3.php
@@ -1,8 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
 error_reporting(E_ALL);
+$form_submitted = $_SERVER["REQUEST_METHOD"] == "POST";
+
+$answerKey = [
+    "q1" => "c", "q2" => "b", "q3" => "a",
+    "q4" => "b", "q5" => "c", "q6" => "a",
+    "q7" => "a", "q8" => "d", "q9" => "a",
+    "q10" => "d"
+];
+
+$questions = [
+    "PHP server scripts are surrounded by delimiters, which?" => [
+        "a" => "&lt;?php> ... &lt;/?>",
+        "b" => "<&> ... &lt;/&>",
+        "c" => "&lt;?php ... ?>",
+        "d" => "&lt;script> ... &lt;/script>"
+    ],
+    "All variables in PHP start with which symbol?" => [
+        "a" => "&",
+        "b" => "$",
+        "c" => "!"
+    ],
+    "How do you get information from a form using the 'get' method?" => [
+        "a" => '$_GET[]',
+        "b" => 'Request.form;',
+        "c" => 'Request.QueryString;'
+    ],
+    "When using the POST method, variables are displayed in the URL:" => [
+        "a" => "True",
+        "b" => "False"
+    ],
+    "What is the correct way to create a function in PHP?" => [
+        "a" => "create myFunction()",
+        "b" => "new_function myFunction()",
+        "c" => "function myFunction()"
+    ],
+    "What is the correct way to open the file 'time.txt' as readable?" => [
+        "a" => 'fopen("time.txt","r");',
+        "b" => 'open("time.txt","read");',
+        "c" => 'open("time.txt");',
+        "d" => 'fopen("time.txt","r+");'
+    ],
+    "PHP allows you to send emails directly from a script" => [
+        "a" => "True",
+        "b" => "False"
+    ],
+    "What is the correct way to add 1 to the \$count variable?" => [
+        "a" => '$count =+1',
+        "b" => '++count',
+        "c" => 'count++;',
+        "d" => '$count++;'
+    ],
+    "The die() and exit() functions do the exact same thing." => [
+        "a" => "True",
+        "b" => "False"
+    ],
+    "Which operator checks if two values are equal and of the same data type?" => [
+        "a" => "==",
+        "b" => "=",
+        "c" => "!=",
+        "d" => "==="
+    ]
+];
+
+for($i = 0; $i < count($questions); $i++) {
+    $img_url[$i] = "blank.jpg";
+}
+
+if ($form_submitted) {
+    $i = 0;
+    foreach ($answerKey as $q => $correctAnswer) {
+        if (isset($_POST[$q]) && $_POST[$q] == $correctAnswer) {
+            $img_url[$i] = "checkmark.png";
+        } else {
+            $img_url[$i] = "xmark.jpg";
+        }
+        $i++;
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <link rel="stylesheet" href="lab3.css">
     <meta charset="UTF-8">
@@ -13,138 +92,36 @@ error_reporting(E_ALL);
 <p>Submitted by Connor Griffin</p>
 <hr>
 
-<?php
-    for($i = 0; $i < 10; $i++) $img_url[$i] = "checkmark.png";
-    $form_submitted = false;
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+    <?php
+    $i = 0;
+    foreach ($questions as $question => $answers) {
+        echo "<div class='question'>";
+        echo "<h3>Question #" . ($i + 1) . " - " . $question . "</h3>";
+        echo "</div>";
 
-$answer_key = [
-    ["q1", "c"], ["q2", "b"], ["q3", "a"],
-    ["q4", "b"], ["q5", "c"], ["q6", "a"],
-    ["q7", "a"], ["q8", "d"], ["q9", "a"],
-    ["q10", "d"]
-];
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $form_submitted = true;
-    $img_url[0] = "checkmark.png";
-
-    $submitted_answers = [
-        $_POST["q1"], $_POST["q2"], $_POST["q3"],
-        $_POST["q4"], $_POST["q5"], $_POST["q6"],
-        $_POST["q7"], $_POST["q8"], $_POST["q9"],
-        $_POST["q10"]
-    ];
-
-    for($i = 0; $i < count($submitted_answers); $i++) {
-        if($submitted_answers[$i] == $answer_key[$i][1]) {
-            $img_url[$i] = "checkmark.png";
-        }else{
-            $img_url[$i] = "xmark.jpg";
+        if ($form_submitted) {
+            echo "<img src='{$img_url[$i]}' alt='Feedback Image'>";
         }
+
+        echo "<br>";
+
+        foreach ($answers as $key => $value) {
+            $checked = "";
+            if(isset($_POST["q". ($i + 1)]) && $_POST["q" . ($i + 1)] == $key){
+                $checked = "checked";
+            }
+            echo "<input type='radio' name='q" . ($i + 1) . "' value='$key' $checked> $value <br>";
+        }
+
+        echo "<br>";
+        $i++;
     }
-}
-?>
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-
-    <div class="question">
-        <h3>Question #1 - PHP server scripts are surrounded by delimiters, which?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[0].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q1" value="a"> &lt;?php> ... &lt;/?> <br>
-    <input type="radio" name="q1" value="b"> &lt;&> ... &lt;/&> <br>
-    <input type="radio" name="q1" value="c"> &lt;?php ... &lt;?> <br>
-    <input type="radio" name="q1" value="d"> &lt;script> ... &lt;script> <br>
-
-    <div class="question">
-        <h3>Question #2 - All variable in PHP start with which symbol?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[1].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q2" value="a"> & <br>
-    <input type="radio" name="q2" value="b"> $ <br>
-    <input type="radio" name="q2" value="c"> ! <br>
-
-    <div class="question">
-        <h3>Question #3 - How do you get information from a form that is submitted using the "get" method?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[2].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q3" value="a"> $_GET[] <br>
-    <input type="radio" name="q3" value="b"> Request.form; <br>
-    <input type="radio" name="q3" value="c"> Request.QueryString; <br>
-
-    <div class="question">
-        <h3>Question #4 - When using the POST method, variables are displayed in the URL:</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[3].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q4" value="a"> True <br>
-    <input type="radio" name="q4" value="b"> False <br>
-
-    <div class="question">
-        <h3>Question #5 - What is the correct way to create a function in PHP?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[4].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q5" value="a"> create myFunction() <br>
-    <input type="radio" name="q5" value="b"> new_function myFunction() <br>
-    <input type="radio" name="q5" value="c"> function myFunction() <br>
-
-    <div class="question">
-        <h3>Question #6 - What is the correct way to open the file "time.txt" as readable?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[5].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q6" value="a"> fopen("time.txt","r"); <br>
-    <input type="radio" name="q6" value="b"> open("time.txt","read"); <br>
-    <input type="radio" name="q6" value="c"> open("time.txt"); <br>
-    <input type="radio" name="q6" value="d"> fopen("time.txt","r+"); <br>
-
-    <div class="question">
-        <h3>Question #7 - PHP allows you to send emails directly from a script</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[6].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q7" value="a"> True <br>
-    <input type="radio" name="q7" value="b"> False <br>
-
-    <div class="question">
-        <h3>Question #8 - What is the correct way to add 1 to the $count variable?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[7].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q8" value="a"> $count =+1 <br>
-    <input type="radio" name="q8" value="b"> ++count <br>
-    <input type="radio" name="q8" value="c"> count++; <br>
-    <input type="radio" name="q8" value="d"> $count++; <br>
-
-    <div class="question">
-        <h3>Question #9 - The die() and exit() functions do the exact same thing.</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[8].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q9" value="a"> True <br>
-    <input type="radio" name="q9" value="b"> False <br>
-
-    <div class="question">
-        <h3>Question #10 - Which operator is used to check if two values are equal and of same data type?</h3>
-        <?php if($form_submitted){ echo "<img src=".$img_url[9].">";} ?>
-    </div>
-    <br>
-    <input type="radio" name="q10" value="a"> == <br>
-    <input type="radio" name="q10" value="b"> = <br>
-    <input type="radio" name="q10" value="c"> != <br>
-    <input type="radio" name="q10" value="d"> === <br>
-
+    ?>
 
     <br>
     <input type="submit">
 </form>
+
 </body>
 </html>
-
-<?php
-
-?>
