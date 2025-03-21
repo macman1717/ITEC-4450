7ebbc5b20c35359386926eba8ab92e078eaa0e52
macman1717 <mchawk1717@gmail.com>
2025-03-21 10:45:35 -0400

    fixed user/quiz nav

--- projects/sp25ClassProject-P1/user/user_nav.php
+++ projects/sp25ClassProject-P1/user/user_nav.php
@@ -1,9 +1,9 @@
 <html>
 <body>
 <p align="center">
-    <a href="user_home.php"> Home </a>
+    <a href="../user/user_home.php"> Home </a>
     |
-    <a href="user_profile.php"> Update Profile </a>
+    <a href="../user/user_profile.php"> Update Profile </a>
     |
     <a href="../quiz/php-quiz.php"> Take PHP Quiz </a>
     |
@@ -11,7 +11,7 @@
     |
     <a href="../quiz/html-css-quiz.php"> Take HTML/CSS Quiz </a>
     |
-    <a href="results.php"> Check Test Result </a>
+    <a href="../user/results.php"> Check Test Result </a>
     |
     <a href="../logout.php"> Logout</a>
 </p>
