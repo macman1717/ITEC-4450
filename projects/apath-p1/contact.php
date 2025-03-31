b2ce3a1d7b471217ec958e9914e96d4dbe51eee9
macman1717 <mchawk1717@gmail.com>
2025-03-31 12:12:24 -0400

    added contact page

---
+++ projects/apath-p1/contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles/index.css">
+    <meta charset="UTF-8">
+    <title>APATH - Home</title>
+</head>
+<body>
+<?php include 'nav.php'; ?>
+<div>
+    <img src="atlanta.png" height="200">
+</div>
+<div id="home-div">
+    <h1>Atlanta, GA</h1>
+    <p>
+        Atlanta, Georgia, is the capital and largest city of the state of Georgia in the U.S. <br>
+        It is known for its rich history, diverse culture, and major role in the civil rights movement. <br>
+        The city is a major economic hub with strong industries in finance, technology, film, and transportation. <br>
+        Atlanta is home to the world's busiest airport, Hartsfield-Jackson Atlanta International Airport, <br>
+        and headquarters for major corporations like Coca-Cola, Delta Airlines, and Home Depot. <br>
+        <br><br>
+
+        The city boasts famous attractions such as the Martin Luther King Jr. National Historical Park, <br>
+        the Georgia Aquarium, the World of Coca-Cola, and the Atlanta BeltLine. It has a vibrant music and <br>
+        arts scene, a strong sports culture with teams like the Atlanta Falcons (NFL), Atlanta Hawks (NBA), and Atlanta <br>
+        Braves (MLB), and is known for its Southern hospitality and diverse culinary scene. <br>
+    </p>
+
+    <h1>About APATH</h1>
+    <p>
+        Our platform connects students arriving in Atlanta with friendly local volunteers for <br>
+        hassle-free airport pickups. Whether you're a new student or a volunteer looking to help, <br>
+        we make it easy to coordinate rides, ensuring a warm welcome and a smooth arrival experience. <br>
+    </p>
+</div>
+
+<footer>
+    <a id='covid-link' href="">Covid information and Guidelines</a>
+</footer>
+</body>
+</html>
